2ad8d7c6568a8d51eacec613ce5a8292e2dcfbf3
marirajan1904 <53859925+marirajan1904@users.noreply.github.com>
2025-11-24 16:36:03 +0530

    Update axios.ts

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -3,7 +3,7 @@ import { store } from "../app/store";
 import { setAccessToken, logout } from "../features/auth/authSlice";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://subscription-dashboard-task-2.onrender.com/api",
   withCredentials: true,
 });
 
